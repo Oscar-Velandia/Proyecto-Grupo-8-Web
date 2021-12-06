fd78da8dd73e31f00cd3a9b84277d1fb93d1bd70
Oscar-Velandia <90572649+Oscar-Velandia@users.noreply.github.com>
2021-12-05 21:48:46 -0500

    Add files via upload

---
+++ back/config/database.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const host = "localhost";
+const port = "27017";
+const db = "hr";
+
+const remoteHost = "cluster0.nwzcb.mongodb.net";
+const remoteUser = "oavelandiaa";
+const remotePass = "oscar";
+const remoteDb = "hr";
+
+
+
+
+
+exports.mongoConnect = () => {
+    //const mongoStringConnection = `mongodb://${host}:${port}/${db}`;
+    const mongoStringConnection = `mongodb+srv://${remoteUser}:${remotePass}@${remoteHost}/${remoteDb}?retryWrites=true&w=majority`;
+    mongoose.connect(mongoStringConnection);
+    mongoose.Promise = global.Promise;
+    const dbConnection = mongoose.connection;
+    dbConnection.on("error", console.error.bind(console,"Mongodb connection error"))
+}
